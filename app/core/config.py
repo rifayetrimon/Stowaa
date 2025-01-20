ca110f038a85b19a06800f9e4643b395852c2503
rifayet-rimon <rifayetrimon88@gmail.com>
2025-01-20 09:21:43 +0800

    add config.py with pydantic schema

---
+++ app/core/config.py
@@ -0,0 +1,16 @@
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    APP_NAME: str = "FastAPI ECOM"
+    DEBUG: bool = True
+    SECRET_KEY: str
+    DATABASE_URL: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+    ALGORITHM: str = "HS256"
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
