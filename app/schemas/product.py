6207e0d39207ae35525c9799c702343b4ab2f8fb
rifayet <rifayetrimon88@gmail.com>
2025-03-03 10:28:09 +0800

    change product functionality

--- app/schemas/product.py
+++ app/schemas/product.py
@@ -1,8 +1,7 @@
 from pydantic import BaseModel, HttpUrl, Field, field_validator, field_serializer
 from typing import Optional, List
 from datetime import datetime
-from app.schemas.category import CategoryResponse
-from app.schemas.review import ReviewResponse
+
 
 class ProductBase(BaseModel):
     name: str = Field(..., min_length=2, max_length=100)
@@ -14,12 +13,14 @@ class ProductBase(BaseModel):
     image_url: Optional[HttpUrl] = None
     is_active: bool = True
 
+
 class ProductCreate(ProductBase):
     @field_validator('sku')
     def validate_sku(cls, v):
+        v = v.upper()
         if ' ' in v:
             raise ValueError("SKU cannot contain spaces")
-        return v.upper()
+        return v
 
 
 class ProductUpdate(BaseModel):
@@ -31,28 +32,25 @@ class ProductUpdate(BaseModel):
     is_active: Optional[bool] = None
 
     @field_serializer('image_url')
-    def serialize_image_url(self, image_url: HttpUrl, _info):
+    def serialize_image_url(self, image_url: Optional[HttpUrl], _info):
         return str(image_url) if image_url else None
 
 
 class ProductResponse(ProductBase):
     id: int
-    user_id: int  # ✅ Only in Response
+    user_id: int
     updated_at: datetime
 
     @field_serializer('updated_at')
     def serialize_updated_at(self, dt: datetime, _info):
         return dt.isoformat()
 
-    class Config:
-        from_attributes = True
-
 
 class ProductListResponse(BaseModel):
     status: str
     message: str
     count: int
-    data: list[ProductResponse]  
+    data: List[ProductResponse]
 
 
 class ProductCreateResponse(BaseModel):
